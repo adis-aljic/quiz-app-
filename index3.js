d7fe40fbc2148d1e625490d2392582898c6564f5
Adis Aljic <adis.qm@gmail.com>
2022-10-20 21:38:15 +0200

    added show more/less btn

--- index3.js
+++ index3.js
@@ -1,3 +1,21 @@
+const show_less = document.getElementById("s_less")
+const show_more = document.getElementById("s_more")
+
+show_less.addEventListener("click", ()=>{
+
+    document.getElementById("card").classList.add("hidden")
+    document.getElementById("s_less").classList.add("hidden")
+    document.getElementById("categories_scroll").classList.remove("hidden")
+    document.getElementById("s_more").classList.remove("hidden")
+})
+show_more.addEventListener("click", ()=>{
+
+    document.getElementById("card").classList.remove("hidden")
+    document.getElementById("s_less").classList.remove("hidden")
+    document.getElementById("categories_scroll").classList.add("hidden")
+    document.getElementById("s_more").classList.add("hidden")
+})
+
 const rules = () => {
     document.getElementById("rules").addEventListener("click", () =>
         alert(`
@@ -8,6 +26,7 @@ const rules = () => {
     Good Luck`)
     )
 }
+
 
 
 
